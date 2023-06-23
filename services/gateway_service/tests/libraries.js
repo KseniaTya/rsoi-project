f56d4ac39403bc644662a09770fb8a186c6f8f52
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 20:24:56 +0300

    2

--- services/gateway_service/tests/libraries.js
+++ services/gateway_service/tests/libraries.js
@@ -23,7 +23,6 @@ $('.btn-libraries').click(function (e) {
               let i=0;
               data.items.forEach(function(el){               
                     $('.libraries').append('<dl><dt>'+ el.name +'</dt> <dd>  <btn class="btn btn-success btn-info-libraries"> Перейти </btn> </dd></dl>');
-
               });  
                     
             }
@@ -35,3 +34,26 @@ $('.btn-libraries').click(function (e) {
    
     
   });
+
+  $(document).on( "click", ".libraries  .btn-info-libraries", function(e) {
+    e.preventDefault();
+  
+    $.get("http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru", function(jwt){
+      
+    var token = jwt;
+    $.ajax({
+      url: 'http://193.233.164.116:8080/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true',
+      type: 'GET',
+      dataType: 'json',
+      headers: {'token': token},
+      success: function(data) {
+      
+        console.log(data);
+              
+      }
+    }); 
+  
+  
+    });
+  });
+  
