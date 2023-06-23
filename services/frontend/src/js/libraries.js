273cf863ef782ca408120caa7d3f25d6d335fc4d
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-24 02:20:55 +0300

    Причесан внешний вид

--- services/frontend/src/js/libraries.js
+++ services/frontend/src/js/libraries.js
@@ -12,16 +12,16 @@ $('.btn-libraries').click(function (e) {
         $.ajax({
             url: 'http://localhost:8080/api/v1/libraries',
             type: 'GET',
-            crossDomain: true,
             dataType: 'json',
             headers: {'token': token},
             success: function (data) {
-                console.log("Кефтемек");
-                console.log(data);
-                let i = 0;
+                $('.libraries').append('<hr class="hr-type-1">');
                 data.items.forEach(function (el) {
-                    $('.libraries').append('<dl><dt>' + el.name + '</dt> <dd>  <btn class="btn btn-success btn-info-libraries"> Перейти </btn> </dd></dl>');
+                    $('.libraries').append('<dl><dt>' + el.name + '</dt> <dd> ' +
+                        ' <btn class="btn btn-success btn-info-libraries"> Перейти </btn> ' +
+                        '</dd></dl><hr class="hr-type-2">');
                 });
+
 
             }
         });
@@ -34,25 +34,26 @@ $('.btn-libraries').click(function (e) {
 
 $(document).on("click", ".libraries  .btn-info-libraries", function (e) {
     e.preventDefault();
-
+    $('.libraries').text(' ');
     $.get("http://localhost:8080/api/v1/authorize?profile=admin&email=admin@admin.ru", function (jwt) {
 
         var token = jwt;
         $.ajax({
             url: 'http://localhost:8080/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true',
             type: 'GET',
-            crossDomain: true,
             dataType: 'json',
             headers: {'token': token},
             success: function (data) {
+                $('.libraries').append('<hr class="hr-type-1">');
 
-                console.log(data);
                 data.items.forEach(function (el) {
                     if (el.availableCount == 0) {
                         $('.libraries').append('<dl><dt>' + el.name + '</dt> <dt>' + el.author + '</dt> <dd>Доступно: ' + el.availableCount + '</dd> </dl>');
                     } else {
                         $('.libraries').append('<dl><dt>' + el.name + '</dt> <dt>' + el.author + '</dt> <dd>Доступно: ' + el.availableCount + '</dd> <dd>  <btn class="btn btn-success "> Забронировать </btn> </dd></dl>');
                     }
+                    $('.libraries').append('<hr class="hr-type-2">');
+
                 });
 
             }
--- services/frontend/src/js/main.js
+++ services/frontend/src/js/main.js
@@ -9,18 +9,20 @@ $('.btn-token').click(function (e) {
             url: 'http://localhost:8080/api/v1/reservations',
             type: 'GET',
             dataType: 'json',
-            crossDomain: true,
             headers: {'token': token},
             success: function (data) {
                 console.log("Кефтемек");
                 console.log(data);
                 let i = 0;
+                $('.books-reservations').append('<hr class="hr-type-1">');
                 data.forEach(function (el) {
+                    $('.books-reservations').append('<dl><dt>' + el.book.name + '</dt> <dd>' + el.book.author + '</dd> <dd>  <input type="hidden" id="tillDate' + i + '" name="tillDate" value= "' + el.tillDate + '"/> <input type="hidden" id="bookUid' + i + '" name="bookUid" value= "' + el.book.bookUid + '"/> <input type="hidden" id="libraryUid' + i + '" name="libraryUid" value= "' + el.library.libraryUid + '"/>');
                     if (el.status == "RENTED") {
-                        $('.books-reservations').append('<dl><dt>' + el.book.name + '</dt> <dd>' + el.book.author + '</dd> <dd>  <input type="hidden" id="tillDate' + i + '" name="tillDate" value= "' + el.tillDate + '"/> <input type="hidden" id="bookUid' + i + '" name="bookUid" value= "' + el.book.bookUid + '"/> <input type="hidden" id="libraryUid' + i + '" name="libraryUid" value= "' + el.library.libraryUid + '"/><btn class="btn btn-success btn-return-book"> Вернуть </btn> </dd></dl>');
+                        $('.books-reservations').append('<btn class="btn btn-success btn-return-book"> Вернуть </btn> </dd></dl>');
                     } else {
-                        $('.books-reservations').append('Нет доступных резерваций!');
+                        $('.books-reservations').append('(Возвращен)');
                     }
+                    $('.books-reservations').append('<hr class="hr-type-2">');
                 });
 
             }
@@ -48,7 +50,6 @@ $(document).on("click", ".books-reservations  .btn-return-book", function (e) {
             url: 'http://localhost:8080/api/v1/reservations/' + bookUid + '/' + libraryUid + '/' + tillDate,
             type: 'POST',
             dataType: 'json',
-            crossDomain: true,
             headers: {'token': token},
             success: function (data) {
                 console.log("dfdf");
