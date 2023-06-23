25e740c1184c10574b9ca5f2e3a0bd8a39bac6a7
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 16:27:25 +0300

    1

--- services/gateway_service/tests/main.js
+++ services/gateway_service/tests/main.js
@@ -15,9 +15,9 @@ $('.btn-token').click(function (e) {
     
     $.ajax({
           url: 'http://193.233.164.116:8080/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true',
-          type: 'get',
+          type: 'post',
           dataType: 'html',
-          headers: {'x-my-header': token},
+          headers: {'token': token},
           success: function(data) {
                   console.log(data);
                   console.log("кефтеме:");
