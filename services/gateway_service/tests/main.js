b4aaa89029771651258720febf400c940eaa1c76
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 18:11:22 +0300

    12

--- services/gateway_service/tests/main.js
+++ services/gateway_service/tests/main.js
@@ -1,39 +1,49 @@
-$(".btn-token").click(function (e) {
+
+$('.btn-token').click(function (e) { 
   e.preventDefault();
   //$('.my-text').load('http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru');
 
   var token2;
-  $.get(
-    "http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru",
-    function (jwt) {
-      var token = jwt;
+  $.get("http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru", function(jwt){
+    
+    var token = jwt;
+    
+    $.ajax({
+          url: 'http://193.233.164.116:8080/api/v1/api/v1/reservations',
+          type: 'GET',
+          dataType: 'json',
+          headers: {'token': token},
+          success: function(data) {
+            console.log("Кефтемек");
+            // data.book.forEach(function(el){
+            //   console.log(data);
+            //   $('.books').append('<dl><dt>'+ el.name +'</dt> <dd>'+ el.author +'</dd></dl>');
+            //   console.log("кефтеме:");
+            // });  
+                  
+          }
+      }); 
 
-      $.ajax({
-        url: "http://193.233.164.116:8080/api/v1/api/v1/reservations",
-        type: "GET",
-        dataType: "json",
-        headers: { token: token },
-        success: function (data) {
-          console.log("Кефтемек");
-          // data.book.forEach(function(el){
-          //   console.log(data);
-          //   $('.books').append('<dl><dt>'+ el.name +'</dt> <dd>'+ el.author +'</dd></dl>');
-          //   console.log("кефтеме:");
-          // });
-        },
-      });
-    }
-  );
 
+  });
+
+ 
   console.log(token2);
+
+ 
+  
 });
 
-//   $.ajax({
-//     url: 'http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru',
-//     type: 'GET',
-//     dataType: 'json',
-//     success: function(token) {
-//             console.log("кефтеме:" + token);
-//             console.log("кефтеме:");
-//     }
-// });
+
+    //   $.ajax({
+    //     url: 'http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru',
+    //     type: 'GET',
+    //     dataType: 'json',
+    //     success: function(token) {
+    //             console.log("кефтеме:" + token);
+    //             console.log("кефтеме:");
+    //     }
+    // }); 
+
+
+    
