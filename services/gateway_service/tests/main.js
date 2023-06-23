378f647c32e62a650658173d68425468fa84ed83
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 15:47:49 +0300

    2

--- services/gateway_service/tests/main.js
+++ services/gateway_service/tests/main.js
@@ -12,21 +12,25 @@ $('.btn-token').click(function (e) {
     console.log("======================"+token2);
     // token2 = data.toString();
 
+    $.get("http://193.233.164.116:8080/api/v1/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true", function(data){
+
+    console.log(data);
+    });
     
 
-    $.ajax({
-          url: 'http://193.233.164.116:8080/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true',
-          type: 'GET',
-          dataType: 'json',
-          //async: false,
-          data: {
-            jwt: token2
-        },
-          success: function(data) {
-                  console.log(data);
-                  console.log("кефтеме:");
-          }
-      }); 
+    // $.ajax({
+    //       url: 'http://193.233.164.116:8080/api/v1/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true',
+    //       type: 'GET',
+    //       dataType: 'json',
+    //       //async: false,
+    //       data: {
+    //         jwt: token2
+    //     },
+    //       success: function(data) {
+    //               console.log(data);
+    //               console.log("кефтеме:");
+    //       }
+    //   }); 
 
 
   });
