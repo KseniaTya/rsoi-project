a1c288f6986c23792b3cfb7a3fd29bca0ce543c3
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 15:14:59 +0300

    1

--- services/gateway_service/tests/main.js
+++ services/gateway_service/tests/main.js
@@ -2,6 +2,10 @@
 $('.btn-token').click(function (e) { 
   e.preventDefault();
   $('.my-text').load('http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru');
+
+  $.get("http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru", function(data, status){
+    alert("Данные: " + data + "\nСостояние: " + status);
+  });
   
 });
 
