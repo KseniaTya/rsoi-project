a6ded15bb55049706dbe4d826d5f134e4254a737
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 16:33:21 +0300

    1

--- services/gateway_service/tests/main.js
+++ services/gateway_service/tests/main.js
@@ -15,11 +15,14 @@ $('.btn-token').click(function (e) {
     
     $.ajax({
           url: 'http://193.233.164.116:8080/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true',
-          type: 'get',
+          type: 'GET',
           dataType: 'json',
+          data: {
+              telephone: 1,
+          },
           headers: {'token': token},
           success: function(data) {
-                  console.log(data.name);
+                  console.log(data);
                   console.log("кефтеме:");
           }
       }); 
