1ccfbc602312af51daf824e361f80064ef338861
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 14:53:32 +0300

    тестовые значения

---
+++ services/gateway_service/tests/main.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function () {
+    // $('#btnLoad').click(function()    {
+       
+    // });
+
+    //$('.my-text').load('http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru');
+
+   
+   // $('.my-text').load('http://193.233.164.116:8080/test?id_test=get_books.php'); 
+
+
+      $.ajax({
+        url: 'http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru',
+        type: 'GET',
+        dataType: 'json',
+        success: function(token) {
+           
+                console.log(token);
+               
+                    // data.fields.forEach(function(field){
+                    //      $(`input[name="${field}"]`).addClass("border-danger");
+                    //      $(`textarea[name="${field}"]`).addClass("border-danger");
+                    //      $(`select[name="${field}"]`).addClass("border-danger");
+                    // });  
+
+                
+                
+                //$('.msg').removeClass('none').text(data.message); //
+            
+        }
+
+
+
+    }); 
+
+      //console.log(yourVariable);
+
+    // $.get("http://193.233.164.116:8080/test?id_test=get_books.php", function(data){
+    //     console.log(data);
+    
+       
+    //   });
+
+
+      
+ 
+});
---
+++ services/gateway_service/tests/test.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Демо Bootstrap</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <header class="d-flex justify-content-center py-3">
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Просмотр книг в </a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Функции</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Ценообразование</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">FAQ</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">О нас</a></li>
+            </ul>
+        </header>
+        <h1>Привет мир!</h1>
+
+        <div class="card" style="width: 18rem;">
+
+            <div class="card-body">
+                <h5 class="card-title">Заголовок карточки</h5>
+                <p class="card-text my-text">Небольшой пример текста, который должен основываться на названии карточки
+                    и
+                    составлять основную часть содержимого карты.</p>
+                <a href="#" class="btn btn-primary">Перейти куда-нибудь</a>
+            </div>
+        </div>
+        <div class="row">
+
+        </div>
+    </div>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+
+    <script src="jquery-3.6.4.min.js">
+    </script>
+
+    <script src="main.js">
+    </script>
+</body>
+
+</html>
