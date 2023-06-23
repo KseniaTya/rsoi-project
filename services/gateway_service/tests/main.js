538e41ca51b017944a25fabd4757c5a742e54248
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 16:11:21 +0300

    1

--- services/gateway_service/tests/main.js
+++ services/gateway_service/tests/main.js
@@ -27,7 +27,7 @@ $('.btn-token').click(function (e) {
 
     $.ajax({
           url: 'http://193.233.164.116:8080/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true',
-          type: 'get',
+          method: 'get',
           dataType: 'json',
           processData: false,
           contentType: false,
