9d4fe5aaa5f9f1f98c674850579231638163f8c9
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 15:17:41 +0300

    1

--- services/gateway_service/tests/main.js
+++ services/gateway_service/tests/main.js
@@ -4,7 +4,7 @@ $('.btn-token').click(function (e) {
   $('.my-text').load('http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru');
 
   let token;
-  $.get("http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru", function(data, status){
+  $.get("http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru", function(data){
     token = data;
   });
 
