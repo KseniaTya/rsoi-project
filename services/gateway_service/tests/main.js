22a095517f7c21b71670b80c167116aebb235328
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 15:28:58 +0300

    1

--- services/gateway_service/tests/main.js
+++ services/gateway_service/tests/main.js
@@ -4,11 +4,26 @@ $('.btn-token').click(function (e) {
   $('.my-text').load('http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru');
 
   var token2;
-  $.get("http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru", function(data){
-    console.log(data);
-    var a = 4;
-    console.log(a);
-    token2 = data.toString();
+  $.get("http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru", function($jwt){
+    // console.log(data);
+    // var a = 4;
+    // console.log(a);
+    // token2 = data.toString();
+
+    $.ajax({
+          url: 'http://193.233.164.116:8080/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true',
+          type: 'GET',
+          dataType: 'json',
+          data: {
+            $jwt: $jwt,
+        },
+          success: function(token) {
+                  console.log("кефтеме:" + token);
+                  console.log("кефтеме:");
+          }
+      }); 
+
+
   });
 
   console.log(a);
