f9cbc7939246b5993a80e7f4a320f5ea23e18f9a
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 15:39:00 +0300

    2

--- services/gateway_service/tests/main.js
+++ services/gateway_service/tests/main.js
@@ -6,9 +6,11 @@ $('.btn-token').click(function (e) {
   var token2;
   $.get("http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru", function(jwt){
     // console.log(data);
-    // var a = 4;
-    console.log(jwt);
+    var token = jwt;
+    console.log(token);
     // token2 = data.toString();
+
+    
 
     $.ajax({
           url: 'http://193.233.164.116:8080/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true',
@@ -16,7 +18,7 @@ $('.btn-token').click(function (e) {
           dataType: 'json',
           //async: false,
           data: {
-            jwt: jwt
+            jwt: token
         },
           success: function(data) {
                   console.log(data);
