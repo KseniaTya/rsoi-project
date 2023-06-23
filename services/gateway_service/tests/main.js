516d152839655f1d1d7acb0da59a833a1b2285c5
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 16:08:00 +0300

    1

--- services/gateway_service/tests/main.js
+++ services/gateway_service/tests/main.js
@@ -20,7 +20,7 @@ $('.btn-token').click(function (e) {
     let formData = new FormData();
     formData.append('token', token);
     
-    $.get("http://193.233.164.116:8080/api/v1/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true", function(data){
+    $.get("http://193.233.164.116:8080/api/v1/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true", {token: token}, function(data){
       console.log(data.page);
       console.log("кефтеме:");
     });
