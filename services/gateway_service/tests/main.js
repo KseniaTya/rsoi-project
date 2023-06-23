93d90e14111220630a1cbfb6ab779847c73a5632
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-06-23 15:54:03 +0300

    12

--- services/gateway_service/tests/main.js
+++ services/gateway_service/tests/main.js
@@ -1,7 +1,7 @@
 
 $('.btn-token').click(function (e) { 
   e.preventDefault();
-  $('.my-text').load('http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru');
+  //$('.my-text').load('http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru');
 
   var token2;
   $.get("http://193.233.164.116:8080/api/v1/authorize?profile=admin&email=admin@admin.ru", function(jwt){
@@ -9,7 +9,7 @@ $('.btn-token').click(function (e) {
     var token = jwt;
     token2 = jwt;
     console.log(token);
-    console.log("======================"+token2);
+    //console.log("======================"+token2);
     // token2 = data.toString();
 
     // $.get("http://193.233.164.116:8080/api/v1/api/v1/libraries/83575e12-7ce0-48ee-9931-51919ff3c9ee/books?page=1&size=25&showAll=true", function(data){
